4ecf0236144bcf44887d1195681d98970c55ca1e
huyugu <huyugu1989@live.cn>
2018-08-26 11:58:54 +0800

    习题3-2-1

---
+++ PYTHON/习题3-2-1.py
@@ -0,0 +1,51 @@
+def print_2(f):
+    f()
+    f()
+
+
+def print_4(f):
+    print_2(f)
+    print_2(f)
+
+
+def print_01():
+    print("+ - - - - ", end="")
+
+
+def print_02():
+    print("+")
+
+
+def print_03():
+    print_4(print_01)
+    print_02()
+
+
+def print_04():
+    print("|         ", end="")
+
+
+def print_05():
+    print("|")
+
+
+def print_06():
+    print_4(print_04)
+    print_05()
+
+
+def print_07():
+    print_4(print_06)
+
+
+def print_08():
+    print_03()
+    print_07()
+
+
+def print_09():
+    print_4(print_08)
+    print_03()
+
+
+print_09()
